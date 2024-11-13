1f1ff6e9bc426420a84a7f2401035548e76095a9
Guillermo Jimenez <guillermo.jimenez@macstore.mx>
2024-11-12 19:02:58 -0600

    Sección SweetAlert creada

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,20 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			defaultLocale: 'es',
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'Español',
+					lang: 'es'
+				},
+			},
 			logo: {
 				light: './src/assets/logos/logo-macstore-dark.svg',
 				dark: './src/assets/logos/logo-macstore-light.svg',
 				replacesTitle: true,
 			},
 			title: 'Dev Docs',
-			// social: {
-			// 	github: 'https://github.com/withastro/starlight',
-			// },
 			sidebar: [
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', slug: 'guides/example' },
-				// 	],
-				// },
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
 				{
 					label: 'Empezando',
 					autogenerate: {
@@ -72,6 +64,7 @@ export default defineConfig({
 			],
 			customCss: [
 				'./src/fonts/font-face.css',
+				'./src/css/root.css',
 				'./src/css/custom.css',
 			],
 		}),
